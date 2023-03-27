25ada18daea946670f44ecd9bd5f8387e3af4d5c
RubenOlander <rubenolander@gmail.com>
2023-03-27 16:12:16 +0200

    imported font-test-works

---
+++ wp-content/themes/kinokoszyk/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action('after_setup_theme', function () {
+    add_theme_support('menus');
+});
+
+function load_tailwind()
+{
+    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+    // Load tailwind.config.js after tailwind has been loaded
+    wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
+}
+add_action('wp_enqueue_scripts', 'load_tailwind');
--- wp-content/themes/kinokoszyk/header.php
+++ wp-content/themes/kinokoszyk/header.php
@@ -1,4 +1,16 @@
-This some text boi
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <!-- Load Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+</head>
+
+<nav class="font-body">
+    asdasd
+</nav>
