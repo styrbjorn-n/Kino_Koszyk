d0b42c2ea55e65535ed5b3ebdd29fe4faffc6efa
Styrbjörn Nordberg <styris.n@gmail.com>
2023-03-28 13:14:28 +0200

    minor menu start

--- wp-content/themes/kinokoszyk/functions.php
+++ wp-content/themes/kinokoszyk/functions.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 add_action('after_setup_theme', function () {
@@ -11,3 +12,17 @@ function load_tailwind()
     wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
 }
 add_action('wp_enqueue_scripts', 'load_tailwind');
+=======
+<?php 
+
+add_action('after_setup_theme', function () {
+  add_theme_support('menus');
+});
+
+function register_theme_menus() {
+  register_nav_menus(array(
+    'primary-menu' => __('Primary Menu'),
+    'footer-menu' => __('Footer Menu'),
+));
+}
+>>>>>>> Stashed changes
