27c6304d621b7041917b1df03809f1b1105a3eb3
Styrbjörn Nordberg <styris.n@gmail.com>
2023-03-28 14:00:19 +0200

    start on kino frontpage

--- wp-content/themes/kinokoszyk/header.php
+++ wp-content/themes/kinokoszyk/header.php
@@ -14,3 +14,5 @@
 <nav class="font-body">
     asdasd
 </nav>
+<body>
+    
--- wp-content/themes/kinokoszyk/index.php
+++ wp-content/themes/kinokoszyk/index.php
@@ -1,2 +1,21 @@
-<?php
-get_header();
+<?php get_header(); ?>
+
+<div class="hero">
+  <h1></h1>
+  <img src="" alt="">
+</div>
+<article class="cultural-entrepreneurs">
+  <h2></h2>
+  <p></p>
+  <img src="" alt="">
+  <div class="link-wrapper"></div>
+</article>
+<section>
+  <h3></h3>
+  <div class="facebook-wrapper"></div>
+</section>
+<section>
+  <h3></h3>
+  <div class="facebook-wrapper"></div>
+</section>
+<?php get_footer(); ?>
