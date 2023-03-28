db6a4b3d20121ff354e4a7cb6fb0b4866bc70a4d
Styrbjörn Nordberg <styris.n@gmail.com>
2023-03-28 15:49:21 +0200

    index styling mostly finnished

--- wp-content/themes/kinokoszyk/index.php
+++ wp-content/themes/kinokoszyk/index.php
@@ -1,21 +1,30 @@
 <?php get_header(); ?>
 
 <div class="hero w-full">
-  <h1>Kino</h1>
-  <img class="w-full aspect-video bg-slate-500 shrink-0" src="" alt="">
+  <h1 id="hero-title" class="text-5xl relative h-fit
+  after:content-['Koszyk'] after:absolute after:left-8 after:top-10
+  ">
+    Kino</h1>
+  <img class="w-full aspect-video bg-gray-400 shrink-0" src="" alt="">
 </div>
-<article class="cultural-entrepreneurs">
-  <h2></h2>
-  <p></p>
-  <img src="" alt="">
-  <div class="link-wrapper"></div>
+<article class="cultural-entrepreneurs mt-8">
+  <h2 class="text-xl">Cultural entrepreneurs</h2>
+  <p class="mt-3">Kino Koszyk HB was founded by Joanna Helander & Bo Persson as an independent company in 1987 with a head office in Gothenburg, Sweden. The company produces creative documentaries for cinema and television, produces books and exhibitions.</p>
+  <img class="w-full h-40 bg-gray-400 mt-8" src="" alt="">
+  <div class="link-wrapper w-full h-40 flex flex-wrap">
+    <a class="w-1/3 h-1/2" href=""><img class="border-2 w-full h-full" src="./foton/svg-icons/image1.svg" alt=""></a>
+    <a class="w-1/3 h-1/2" href=""><img class="border-2 w-full h-full" src="./foton/svg-icons/image1.svg" alt=""></a>
+    <a class="w-1/3 h-1/2" href=""><img class="border-2 w-full h-full" src="./foton/svg-icons/image1.svg" alt=""></a>
+    <a class="w-1/2 h-1/2" href=""><img class="border-2 w-full h-full" src="./foton/svg-icons/image1.svg" alt=""></a>
+    <a class="w-1/2 h-1/2" href=""><img class="border-2 w-full h-full" src="./foton/svg-icons/image1.svg" alt=""></a>
+  </div>
 </article>
 <section>
-  <h3></h3>
-  <div class="facebook-wrapper"></div>
+  <h3 class="text-2xl">Joanna Helander</h3>
+  <div class="facebook-wrapper w-full aspect-square bg-gray-400"></div>
 </section>
 <section>
-  <h3></h3>
-  <div class="facebook-wrapper"></div>
+  <h3 class="text-2xl">Bo persson</h3>
+  <div class="facebook-wrapper w-full aspect-square bg-gray-400"></div>
 </section>
 <?php get_footer(); ?>
