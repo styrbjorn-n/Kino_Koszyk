b3a9accbf73e6d1aac3f9d75437e36f7c81a178e
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-03-28 14:33:48 +0200

    custom post types + archive
    added archive-film.php and single-film.php
    Began making the custom post type in functions.php

--- wp-content/themes/kinokoszyk/functions.php
+++ wp-content/themes/kinokoszyk/functions.php
@@ -1,13 +1,32 @@
 <?php
 
 add_action('after_setup_theme', function () {
-    add_theme_support('menus');
+  add_theme_support('menus');
 });
 
 function load_tailwind()
 {
-    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
-    // Load tailwind.config.js after tailwind has been loaded
-    wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
+  wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+  // Load tailwind.config.js after tailwind has been loaded
+  wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
 }
 add_action('wp_enqueue_scripts', 'load_tailwind');
+
+
+function create_posttype()
+{
+
+  register_post_type(
+    'movies',
+    array(
+      'labels' => array(
+        'name' => __('Movies'),
+        'singular_name' => __('Movie')
+      ),
+      'public' => true,
+      'has_archive' => true,
+
+    )
+  );
+}
+add_action('init', 'create_posttype');
