5bc4d8780ade1f420d70c14b178519c0bffb169f
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-04-03 11:56:28 +0200

    added test posttypes + changes to archive.film

--- wp-content/themes/kinokoszyk/archive-film.php
+++ wp-content/themes/kinokoszyk/archive-film.php
@@ -1,15 +1,42 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : ?>
-    <div>
+
+
+    <div class="w-full flex flex-wrap justify-evenly lg:gap-8">
         <!-- This is a card in the grid -->
         <?php while (have_posts()) : the_post(); ?>
 
-            <a href=<?php the_permalink() ?>> <?= the_title(); ?> </a>
+            <?php
+            $image = get_field('film_image');
+            if ($image) :
+
+                // Image variables.
+                // $url = the_permalink();
+                $title = $image['title'];
+                $alt = $image['title'];
+                $caption = $image['caption'];
+
+                // Thumbnail size attributes.
+                $size = 'large';
+                $thumb = $image['sizes'][$size];
+                $width = $image['sizes'][$size . '-width'];
+                $height = $image['sizes'][$size . '-height'];
+            ?>
+                <div class="flex flex-col lg:w-1/4 w-">
+                    <a href="<?php the_permalink(); ?>" title="<?= esc_attr($title); ?>">
+                        <img class="object-cover aspect-[8/12] w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                    </a>
+                    <a class="whitespace-break-spaces" href=<?php the_permalink() ?>> <?= the_title(); ?> </a>
+                </div>
+
+            <?php endif; ?>
+
             <!-- EOF: card in the grid -->
         <?php endwhile; ?>
+    </div>
 
-        <!-- EOF: the grid -->
-    <?php endif; ?>
+    <!-- EOF: the grid -->
+<?php endif; ?>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
