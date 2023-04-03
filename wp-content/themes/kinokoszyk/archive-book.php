317375d8db5627cfe9623ab4d8d1f280ef6e5566
filjon2001 <filip.jonasson@hotmail.com>
2023-04-03 16:24:50 +0200

    Starting with archive books

--- wp-content/themes/kinokoszyk/archive-book.php
+++ wp-content/themes/kinokoszyk/archive-book.php
@@ -9,6 +9,31 @@
             <!-- EOF: card in the grid -->
         <?php endwhile; ?>
 
+        <?php
+        $image = get_field('book_image');
+        if ($image) :
+
+            // Image variables.
+            // $url = the_permalink();
+            $title = $image['title'];
+            $alt = $image['title'];
+            $caption = $image['caption'];
+
+            // Thumbnail size attributes.
+            $size = 'large';
+            $thumb = $image['sizes'][$size];
+            $width = $image['sizes'][$size . '-width'];
+            $height = $image['sizes'][$size . '-height'];
+        ?>
+            <div class="flex flex-col lg:w-1/4 w-1/2 pr-2">
+                <a href="<?php the_permalink(); ?>" title="<?= esc_attr($title); ?>">
+                    <img class="object-cover aspect-[8/12] w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                </a>
+                <a class="whitespace-break-spaces sm: text-mobileP lg: text-desktopP md: text-tabletP" href=<?php the_permalink() ?>> <?= the_title(); ?> </a>
+            </div>
+
+        <?php endif; ?>
+
         <!-- EOF: the grid -->
     <?php endif; ?>
 
