63b12e59af5abf8c9d10369b8dff24c2bc2b239b
Styrbjörn Nordberg <styris.n@gmail.com>
2023-04-03 13:28:56 +0200

    mostly finnished styling of front page

--- wp-content/themes/kinokoszyk/footer.php
+++ wp-content/themes/kinokoszyk/footer.php
@@ -1,10 +1,10 @@
 <?php wp_footer(); ?>
 
-<footer class="text-white h-footer bg-black py-24 px-5 md:pl-16 mt-16">
+<footer class="h-footer bg-black py-24 px-5 md:pl-16 mt-16">
     <div class="flex gap-10 md:gap-36">
         <img src="<?= get_template_directory_uri(); ?>/src/logowhite.svg" alt="Kinokoszyk Logo" class="w-28 md:w-52" />
-        <div class="text-white font-text font-normal flex flex-col">
-            <p class="mb-2">Kino Koszyk HB</p>
+        <div class="font-normal flex flex-col">
+            <p class="text-white mb-2">Kino Koszyk HB</p>
             <p>kino.koszyk@gmail.com</p>
             <p>+46 768 32 74 55</p>
             <small class="justify-self-end mt-2">Copyright &copy; 2023 </small>
--- wp-content/themes/kinokoszyk/front-page.php
+++ wp-content/themes/kinokoszyk/front-page.php
@@ -5,21 +5,16 @@
 
     <div class="w-full">
         <h1 id="hero-title" class="relative text-black h-fit text-5xl lg:text-9xl"> Kino
-            <span class="ml-5 lg:absolute text-gray-500 lg:left-10 lg:top-[7rem]">Koszyk</span>
+            <span class="ml-5 lg:absolute opacity-60 lg:left-10 lg:top-[7rem]">Koszyk</span>
         </h1>
         <img class="w-full aspect-video bg-gray-400 shrink-0" src="" alt="">
     </div>
     <article class="mt-16 flex flex-col md:flex-row h-fit">
-
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div class="flex flex-col basis-3/5 mr-8 text-2xl"><?php the_content(); ?>
-                    <h2><?php the_title() ?></h2>
-                    <p><?php ?></p>
-                </div>
-                <img class="w-full self-start basis-2/5" src="<?= get_template_directory_uri(); ?>/src/logoblack.svg" alt="">
-
-            <?php endwhile; ?>
-        <?php endif; ?>
+        <div>
+            <h2 class="text-xl font-semibold mb-3">Cultural entrepreneurs</h2>
+            <p class="text-base"><?php the_field('front_page_description'); ?></p>
+        </div>
+        <img class="mt-3 w-full self-start basis-2/5" src="<?= get_template_directory_uri(); ?>/src/logoblack.svg" alt="">
     </article>
     <article class="link-wrapper w-full h-fit flex flex-wrap mt-8 mb-16">
         <div class="h-30 w-full flex flex-wrap">
