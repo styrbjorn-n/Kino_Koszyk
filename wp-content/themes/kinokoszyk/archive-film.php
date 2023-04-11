8c9e515c74609c4fd0d1cef725749f2ec2840d28
RubenOlander <rubenolander@gmail.com>
2023-04-11 16:25:09 +0200

    pagination+minoradds

--- wp-content/themes/kinokoszyk/archive-film.php
+++ wp-content/themes/kinokoszyk/archive-film.php
@@ -1,48 +1,59 @@
 <?php get_header(); ?>
-<div class="breadcrumbs uppercase text-grey lg:text-desktopLink md:text-tabletLink sm:text-mobileLink" typeof="BreadcrumbList" vocab="https://schema.org/">
-    <?php if (function_exists('bcn_display')) {
-        bcn_display();
-    } ?>
-</div>
-<h1 class="font-heading text-mobileH1 lg:text-desktopH1 md:text-tabletH1">FILMS</h1>
-<?php if (have_posts()) : ?>
 
-
-    <div class="w-full flex flex-wrap justify-evenly pl-2 lg:gap-8">
-        <!-- This is a card in the grid -->
-        <?php while (have_posts()) : the_post(); ?>
-
-            <?php
-            $image = get_field('film_image');
-            if ($image) :
-
-                // Image variables.
-                // $url = the_permalink();
-                $title = $image['title'];
-                $alt = $image['title'];
-                $caption = $image['caption'];
-
-                // Thumbnail size attributes.
-                $size = 'large';
-                $thumb = $image['sizes'][$size];
-                $width = $image['sizes'][$size . '-width'];
-                $height = $image['sizes'][$size . '-height'];
-            ?>
-                <div class="flex flex-col w-1/2 pr-2 lg:w-1/4">
-                    <a href="<?php the_permalink(); ?>" title="<?= esc_attr($title); ?>">
-                        <img class="object-cover aspect-[8/12] w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
-                    </a>
-                    <a class="font-bold text-mobileP lg:text-desktopP md:text-tabletP" href=<?php the_permalink() ?>> <?= the_title(); ?></a>
-                    <p class="text-mobileP lg:text-desktopP md:text-tabletP"><?= get_field('year'); ?></p>
-                </div>
-
-            <?php endif; ?>
-
-            <!-- EOF: card in the grid -->
-        <?php endwhile; ?>
+<section class="font-heading mx-2 md:mx-16 lg:mx-[120px]">
+    <div class="breadcrumbs uppercase text-grey lg:text-desktopLink md:text-tabletLink sm:text-mobileLink" typeof="BreadcrumbList" vocab="https://schema.org/">
+        <?php if (function_exists('bcn_display')) {
+            bcn_display();
+        } ?>
     </div>
+    <h1 class="uppercase font-black lg:mb-16 lg:text-desktopH1 "><?php post_type_archive_title(); ?></h1>
+    <?php if (have_posts()) : ?>
 
-    <!-- EOF: the grid -->
-<?php endif; ?>
+
+        <div class="w-full flex flex-wrap justify-between">
+            <!-- This is a card in the grid -->
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php
+                $image = get_field('film_image');
+                if ($image) :
+
+                    // Image variables.
+                    // $url = the_permalink();
+                    $title = $image['title'];
+                    $alt = $image['title'];
+                    $caption = $image['caption'];
+
+                    // Thumbnail size attributes.
+                    $size = 'large';
+                    $thumb = $image['sizes'][$size];
+                    $width = $image['sizes'][$size . '-width'];
+                    $height = $image['sizes'][$size . '-height'];
+                ?>
+                    <div class="flex flex-col w-1/2 pr-2 lg:w-1/4">
+                        <a href="<?php the_permalink(); ?>" title="<?= esc_attr($title); ?>">
+                            <img class="object-cover aspect-[8/12] w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                        </a>
+                        <a class="font-bold text-mobileP lg:text-desktopP md:text-tabletP" href=<?php the_permalink() ?>> <?= the_title(); ?></a>
+                        <p class="text-mobileP lg:text-desktopP md:text-tabletP"><?= get_field('year'); ?></p>
+                    </div>
+
+                <?php endif; ?>
+
+                <!-- EOF: card in the grid -->
+            <?php endwhile; ?>
+        </div>
+        <section class="flex flex-row justify-center">
+            <?php echo the_pagination(array(
+                'prev_text'    => ' < ',
+                'next_text'    => ' > '
+            ));
+            ?>
+        </section>
+
+        <!-- EOF: the grid -->
+    <?php endif; ?>
+
+</section>
 
 <?php get_footer(); ?>
