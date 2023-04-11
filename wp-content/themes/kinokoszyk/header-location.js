42a40febf6e1b1bcf47efd0e53baa0a1d278a00f
Styrbjörn Nordberg <styris.n@gmail.com>
2023-04-11 13:52:44 +0200

    made better looking fuction

--- wp-content/themes/kinokoszyk/header-location.js
+++ wp-content/themes/kinokoszyk/header-location.js
@@ -4,26 +4,24 @@ function primaryMenuList(joannaPages, URL) {
   for (let i = 0; i < joannaPages.length; i++) {
     if (URL.includes(joannaPages[i])) {
       children[1].classList.add('text-red-600');
-      children[0].classList.remove('text-red-600');
-      break;
-    } else {
-      children[0].classList.add('text-red-600');
-      children[1].classList.remove('text-red-600');
+      return;
     }
   }
+  children[0].classList.add('text-red-600');
 }
 
 function navMenuList(joannaPages, kinoPages, URL) {
   const children = document.querySelector('.nav-menu-list').children;
+
   for (let i = 0; i < joannaPages.length; i++) {
     if (URL.includes(joannaPages[i]) && i > 4) {
       children[i].classList.add('text-red-600');
       return;
     }
   }
-  for (let i = 0; i < kinoPages.length; i++){
+  for (let i = 0; i < kinoPages.length; i++) {
     if (URL.includes(kinoPages[i])) {
-      children[i].classList.add('text-red-600')
+      children[i].classList.add('text-red-600');
     }
   }
 }
