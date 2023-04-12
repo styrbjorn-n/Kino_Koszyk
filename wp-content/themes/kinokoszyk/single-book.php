d12995ef8b26811a5c63a06729ed4a4455c359db
filjon2001 <filip.jonasson@hotmail.com>
2023-04-12 15:13:05 +0200

    Starting with single-book etc.

--- wp-content/themes/kinokoszyk/single-book.php
+++ wp-content/themes/kinokoszyk/single-book.php
@@ -15,10 +15,17 @@ $fields = get_field_objects(); ?>
 
         <!-- <?php the_breadcrumb() ?> -->
 
-        <h2 class="text-4xl"><?php the_title(); ?></h2>
+        <h1 class="font-black mb-16 mx-2 md:mx-16 font-heading text-mobileH1 lg:text-desktopH1 md:text-tabletH1"><?php post_type_archive_title() ?>Books</h1>
+
+        <h2 class="mx-2 md:mx-16 text-mobileH4 lg:text-desktopH4 md:text-tabletH4"><?php the_title(); ?></h2>
+
+        <p class="mx-2 md:mx-16 text-mobile lg:text-desktop md:text-tablet"><?= get_field('year') ?></p>
+        <p class="mx-2 md:mx-16 text-mobile lg:text-desktop md:text-tablet"><?= get_field('language') ?></p>
+        <p class="mx-2 md:mx-16 text-mobile lg:text-desktop md:text-tablet"><?= get_field('writer') ?></p>
+
+        <p class="m-8 mx-2 md:mx-16 text-mobile lg:text-desktop md:text-tablet"><?= "Publisher: ", get_field('publisher'), "<br>", "ISBN: ", get_field('isbn'); ?></p>
 
         <p class="text-xl"> <?php the_content(); ?> </p>
-
 
     <?php endwhile; ?>
 
