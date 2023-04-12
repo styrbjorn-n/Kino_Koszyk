2884f1587dbe522df091caf7552435721e59eabb
filjon2001 <filip.jonasson@hotmail.com>
2023-04-12 17:14:04 +0200

    Fixing KINO KOSZYK-title responsitivity

--- wp-content/themes/kinokoszyk/front-page.php
+++ wp-content/themes/kinokoszyk/front-page.php
@@ -6,7 +6,7 @@
 <section class="font-heading mx-2 md:mx-16 lg:mx-[120px]">
 
     <div class="w-full">
-        <h1 id="hero-title" class="lg:text-desktopH1 font-black relative z-1 -mb-[11px]"> KINO KOSZYK </h1>
+        <h1 id="hero-title" class="lg:text-desktopH1 md:text-tabletH1 max-sm:text-mobileH1 sm:text-mobileH1 font-black relative z-1 max-sm:-mb-[4px] sm:-mb-[4px] md:-mb-[8px] lg:-mb-[11px]"> KINO KOSZYK </h1>
         <img class="w-full aspect-video bg-gray-400 shrink-0" src="<?= get_template_directory_uri(); ?>/src/hero.jpg" alt="">
     </div>
     <article class="mt-16 h-fit flex flex-col md:flex-row md:gap-4">
@@ -28,7 +28,7 @@
     </article>
     <section>
         <h3 class="text-desktopH3 font-text font-light mb-4">Follow us on Facebook for all the latest news!</h3>
-        <div class="flex flex-wrap justify-center md:flex-nowrap md:gap-4 lg:justify-around">
+        <div class="flex flex-wrap justify-around md:flex-nowrap md:gap-4 lg:justify-around">
             <section class="w-full md:w-1/2 lg:w-[500px] shrink-1 md:shrink-0 basis-1/2">
                 <h2 class="text-3xl md:text-4xl lg:text-desktopH2 font-semibold">Joanna Helander</h2>
                 <div class="fb-page facebook-wrapper w-full aspect-square" data-href="https://www.facebook.com/kinokoszyk/" data-tabs="timeline" data-width="1000" data-height="600" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
