14b253f8293e4291d8c2cec1a603f592975689a7
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-04-13 11:38:10 +0200

    Update header.php

--- wp-content/themes/kinokoszyk/header.php
+++ wp-content/themes/kinokoszyk/header.php
@@ -35,10 +35,8 @@
                 <div>
                     <a href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/src/logoblack.svg" alt="Kino Koszyk Logo" class="w-12 "></a>
                 </div>
-                <<<<<<< HEAD <div class="primary-menu w-full lg:w-fit z-20 lg:z-0 justify-self-stretch max-md:ml-2">
-                    =======
+                <div class="primary-menu w-full lg:w-fit z-20 lg:z-0 justify-self-stretch max-md:ml-2">
                     <div class="primary-menu w-full lg:w-fit z-30 bg-white lg:z-0 justify-self-stretch">
-                        >>>>>>> d240ee89ef3b11f6ffa968797097bcf5ced7125e
                         <!-- writes out the menu items specific to the selected menu -->
                         <?php wp_nav_menu(array(
                             'theme_location' => 'primary-menu', // pages needed: joanna and home or kino
@@ -50,31 +48,31 @@
                         )) ?>
 
                     </div>
-            </div>
-            <div id="nav-links" class="flex flex-col absolute bg-white z-20 top-0 left-[-180%] w-[120%] h-screen mt-12 opacity-80 backdrop-blur duration-500
+                </div>
+                <div id="nav-links" class="flex flex-col absolute bg-white z-20 top-0 left-[-180%] w-[120%] h-screen mt-12 opacity-80 backdrop-blur duration-500
         lg:z-0 lg:h-fit lg:static lg:mt-0 lg:flex-row lg:justify-between lg:justify-self-end lg:w-fit">
 
-                <?php
+                    <?php
 
-                if (is_joanna_page()) {
-                    $menu = 'joanna-menu';
-                } else {
-                    $menu = 'kino-menu';
-                }
-                wp_nav_menu(array(
-                    'theme_location' => $menu,
-                    'container' => false,
-                    'menu_class' => 'nav-menu-list flex flex-col items-center h-full justify-evenly font-bold lg:gap-1 lg:flex-row lg:justify-end lg:gap-8',
-                    'add_li_class' => 'hover:text-hover text-mobileMenu md:text-tabletMenu w-fit'
-                ));
-                ?>
-            </div>
-            <div onclick="onToggleMenu(this)" class="w-[30px] h-[30px] cursor-pointer lg:hidden z-20 lg:z-0">
-                <div id="line-container" class="w-full h-full space-y-2">
-                    <div class="w-full h-0.5 bg-gray-600"></div>
-                    <div class="w-full h-0.5 bg-gray-600"></div>
-                    <div class="w-full h-0.5 bg-gray-600"></div>
+                    if (is_joanna_page()) {
+                        $menu = 'joanna-menu';
+                    } else {
+                        $menu = 'kino-menu';
+                    }
+                    wp_nav_menu(array(
+                        'theme_location' => $menu,
+                        'container' => false,
+                        'menu_class' => 'nav-menu-list flex flex-col items-center h-full justify-evenly font-bold lg:gap-1 lg:flex-row lg:justify-end lg:gap-8',
+                        'add_li_class' => 'hover:text-hover text-mobileMenu md:text-tabletMenu w-fit'
+                    ));
+                    ?>
                 </div>
-            </div>
+                <div onclick="onToggleMenu(this)" class="w-[30px] h-[30px] cursor-pointer lg:hidden z-20 lg:z-0">
+                    <div id="line-container" class="w-full h-full space-y-2">
+                        <div class="w-full h-0.5 bg-gray-600"></div>
+                        <div class="w-full h-0.5 bg-gray-600"></div>
+                        <div class="w-full h-0.5 bg-gray-600"></div>
+                    </div>
+                </div>
         </nav>
     </header>
