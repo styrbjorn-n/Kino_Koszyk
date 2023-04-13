8e22959630dda6d9d51cc7c77ad57b479f3c4316
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-04-13 12:56:15 +0200

    fixed header flexing

--- wp-content/themes/kinokoszyk/header.php
+++ wp-content/themes/kinokoszyk/header.php
@@ -14,43 +14,29 @@
 </head>
 
 <body>
-    <header class="font-heading mb-16 py-6  px-1 lg:mx-3 md:px-3 sticky top-0 bg-white z-10 w-full">
-
-        <nav>
-            <div class="primary-menu hidden">
-                <?php $primaryMenuItems = wp_get_nav_menu_items('Primary');
-                print_menu_items($primaryMenuItems); ?>
-
-            </div>
-            <div class="switching-menu hidden">
-                <?php $kinoMenuItems = wp_get_nav_menu_items('Kino Header Menu');
-                print_menu_items($kinoMenuItems);
-                ?>
-            </div>
-        </nav>
-
+    <header class="font-heading mb-16 py-6 lg:px-6 px-1 md:px-3 sticky top-0 bg-white z-10 w-full">
         <!-- a nav solution -->
         <nav class="burger-menu flex flex-row items-center flex-nowrap w-full lg:justify-between">
-            <div class="flex flex-nowrap md:gap-2 lg:gap-8 w-full">
-                <div>
-                    <a href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/src/logoblack.svg" alt="Kino Koszyk Logo" class="w-12 "></a>
-                </div>
-                <div class="primary-menu w-full lg:w-fit z-20 lg:z-0 justify-self-stretch max-md:ml-2">
-                    <div class="primary-menu w-full lg:w-fit z-30 bg-white lg:z-0 justify-self-stretch">
-                        <!-- writes out the menu items specific to the selected menu -->
-                        <?php wp_nav_menu(array(
-                            'theme_location' => 'primary-menu', // pages needed: joanna and home or kino
-                            'container' => false,
-                            'menu_class' => 'primary-menu-list flex flex-row items-center h-full max-md:justify-center lg:ml-6 gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
-                            'menu_class' => 'primary-menu-list flex flex-row items-center h-full justify-center mb:gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
-                            'add_li_class' => 'hover:text-hover text-xl',
-                            'after' => '<span class="mx-[12px] text-primary"> | </span>'
-                        )) ?>
+            <div class="flex flex-nowrap md:gap-2 lg:gap-8 w-full justify-between">
+                <div class="flex gap-12">
+                    <a href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/src/logoblack.svg" alt="Kino Koszyk Logo" class="w-12"></a>
 
+                    <div class="primary-menu w-full lg:w-fit z-20 lg:z-0 justify-self-stretch max-md:ml-2">
+                        <div class="primary-menu w-full lg:w-fit z-30 bg-white lg:z-0 justify-self-stretch">
+                            <!-- writes out the menu items specific to the selected menu -->
+                            <?php wp_nav_menu(array(
+                                'theme_location' => 'primary-menu', // pages needed: joanna and home or kino
+                                'container' => false,
+                                'menu_class' => 'primary-menu-list flex flex-row items-center h-full max-md:justify-center lg:ml-6 gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
+                                'menu_class' => 'primary-menu-list flex flex-row items-center h-full justify-center mb:gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
+                                'add_li_class' => 'hover:text-hover text-desktopLink',
+                                'after' => '<span class="mx-[12px] text-primary"> | </span>'
+                            )) ?>
+
+                        </div>
                     </div>
                 </div>
-                <div id="nav-links" class="flex flex-col absolute bg-white z-20 top-0 left-[-180%] w-[120%] h-screen mt-12 opacity-80 backdrop-blur duration-500
-        lg:z-0 lg:h-fit lg:static lg:mt-0 lg:flex-row lg:justify-between lg:justify-self-end lg:w-fit">
+                <div id="nav-links" class="flex flex-col absolute bg-white z-20 top-0 left-[-180%] w-[120%] h-screen mt-12 opacity-80 backdrop-blur duration-500 lg:z-0 lg:h-fit lg:static lg:mt-0 lg:flex-row lg:justify-between lg:justify-self-end lg:w-fit">
 
                     <?php
 
@@ -73,5 +59,7 @@
                         <div class="w-full h-0.5 bg-gray-600"></div>
                         <div class="w-full h-0.5 bg-gray-600"></div>
                     </div>
+                </div>
+            </div>
         </nav>
     </header>
