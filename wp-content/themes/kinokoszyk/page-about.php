d9a5a93d091acbde3e225a85e72e5d7861fc6fcc
filjon2001 <filip.jonasson@hotmail.com>
2023-04-13 14:57:27 +0200

    Editing images and headings

--- wp-content/themes/kinokoszyk/page-about.php
+++ wp-content/themes/kinokoszyk/page-about.php
@@ -6,7 +6,10 @@
             bcn_display();
         } ?>
     </div>
-    <h1 class="font-heading text-desktopH1 uppercase"><?php the_title(); ?></h1>
+    <h1 class="font-heading font-black text-desktopH1 mb-16"><?php the_title(); ?></h1>
+    <div class="w-full border-black">
+        <img class="w-full h-full bg-gray-400 shrink-0" src="<?= get_template_directory_uri(); ?>/src/filmbilder/Kobieta (Women)/8_mars_kvinnodagen2 1.png" alt="">
+    </div>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class=""><?php the_content(); ?></div>
         <?php endwhile; ?>
