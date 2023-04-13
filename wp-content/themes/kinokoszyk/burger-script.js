593b9b3dd28dde8cab7b5f3d92dff89456a24c9c
Styrbjörn Nordberg <styris.n@gmail.com>
2023-04-13 11:09:12 +0200

    partial style fixes

--- wp-content/themes/kinokoszyk/burger-script.js
+++ wp-content/themes/kinokoszyk/burger-script.js
@@ -2,7 +2,7 @@ function onToggleMenu(e) {
   const navLinks = document.querySelector('#nav-links');
   const lines = document.querySelector('#line-container').children;
   e.name = e.name === 'menu' ? 'close' : 'menu';
-  navLinks.classList.toggle('translate-x-full');
+  navLinks.classList.toggle('translate-x-[140%]');
 
   // moves burger icon lines in to an x
   lines[0].classList.toggle('rotate-45');
