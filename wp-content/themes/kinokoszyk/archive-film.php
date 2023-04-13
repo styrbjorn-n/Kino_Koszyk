9d1b3162d6b93e263f322bd05fb00078926be262
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-04-13 15:34:50 +0200

    styled archive film

--- wp-content/themes/kinokoszyk/archive-film.php
+++ wp-content/themes/kinokoszyk/archive-film.php
@@ -7,7 +7,7 @@
         } ?>
     </div>
     <section class="font-heading mx-2 md:mx-16 lg:mx-[120px]">
-        <h1 class="mt-0 uppercase font-black lg:mb-16 lg:text-desktopH1 md:text-tabletH1 text-mobileH1"><?php post_type_archive_title(); ?></h1>
+        <h1 class="mt-0 uppercase font-black md:mb-16 mb-4 lg:text-desktopH1 md:text-tabletH1 text-mobileH1"><?php post_type_archive_title(); ?></h1>
         <?php if (have_posts()) : ?>
 
 
@@ -19,23 +19,21 @@
                     $image = get_field('film_image');
                     if ($image) :
 
-                        // Image variables.
-                        // $url = the_permalink();
                         $title = $image['title'];
                         $alt = $image['title'];
                         $caption = $image['caption'];
 
-                        // Thumbnail size attributes.
                         $size = 'large';
                         $thumb = $image['sizes'][$size];
                         $width = $image['sizes'][$size . '-width'];
                         $height = $image['sizes'][$size . '-height'];
                     ?>
-                        <div class="flex flex-col w-1/2 pr-2 lg:w-[32%] : ">
+
+                        <div class="flex flex-col w-[48%] lg:w-[30%] font-text mb-16">
                             <a href="<?php the_permalink(); ?>" title="<?= esc_attr($title); ?>">
                                 <img class="object-cover aspect-[8/12] w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
                             </a>
-                            <a class="font-bold text-mobileP lg:text-desktopP md:text-tabletP" href=<?php the_permalink() ?>> <?= the_title(); ?></a>
+                            <a class="font-bold text-mobileP lg:text-desktopP md:text-tabletP mt-4" href=<?php the_permalink() ?>> <?= the_title(); ?></a>
                             <p class="text-mobileP lg:text-desktopP md:text-tabletP"><?= get_field('year'); ?></p>
                         </div>
 
@@ -44,7 +42,7 @@
                     <!-- EOF: card in the grid -->
                 <?php endwhile; ?>
             </div>
-            <section class="flex text-desktopP flex-row justify-center">
+            <section class="font-text flex text-desktopP flex-row justify-center">
                 <div class="mx-2 mt-16 mb-28 ">
                     <?php echo the_pagination(array(
                         'prev_text'    => ' < ',
