9ce53c2b630c6113015c95c7946b92760a4a2ac7
RubenOlander <rubenolander@gmail.com>
2023-05-05 15:28:34 +0200

    fix

--- wp-content/themes/kinokoszyk/page-biography.php
+++ wp-content/themes/kinokoszyk/page-biography.php
@@ -6,6 +6,17 @@
             bcn_display();
         } ?>
     </div>
+
+    <!-- THIS IS NOT THE CORRECT STYLED MENU, ONLY FOR PLACE HOLDER -->
+    <?php wp_nav_menu(array(
+        'theme_location' => 'joanna-sub-menu',
+        'container' => false,
+        'menu_class' => 'xs:whitespace-nowrap xs:flex-col primary-menu-list flex flex-row items-center h-full max-md:justify-center lg:ml-6 gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
+        'menu_class' => 'xs:whitespace-nowrap xs:flex-col primary-menu-list flex flex-row items-center h-full justify-center mb:gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
+        'add_li_class' => 'hover:text-hover text-desktopLink',
+        'after' => '<span class="max-sm:mr-[8px] sm:mx-[12px] text-primary"> | </span>'
+    )) ?>
+
     <h1 class="font-heading uppercase font-black mb-16 lg:text-desktopH1 md:text-tabletH1 max-md:mb-4 text-mobileH1"><?php the_title(); ?></h1>
     <div class="w-full">
         <img class="w-full h-full shrink-0" src="<?= get_template_directory_uri(); ?>/src/biography.png" alt="Joanna Helander">
