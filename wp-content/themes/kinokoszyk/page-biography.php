8b8a15cc3c3bd0cc3b632d985831af94f07f8574
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-05-08 17:01:51 +0200

    Update page-biography.php

--- wp-content/themes/kinokoszyk/page-biography.php
+++ wp-content/themes/kinokoszyk/page-biography.php
@@ -1,4 +1,6 @@
-<?php get_header(); ?>
+<?php get_header();
+$slug = get_last_url_slug();
+?>
 
 <section class="font-heading mx-2 md:mx-16 lg:mx-[120px]">
     <div class="flex relative breadcrumbs font-text uppercase text-grey lg:text-desktopLink md:text-tabletLink sm:text-mobileLink" typeof="BreadcrumbList" vocab="https://schema.org/">
@@ -8,14 +10,14 @@
     </div>
 
     <!-- THIS IS NOT THE CORRECT STYLED MENU, ONLY FOR PLACE HOLDER -->
+
     <?php wp_nav_menu(array(
         'theme_location' => 'joanna-sub-menu',
         'container' => false,
-        'menu_class' => 'xs:whitespace-nowrap xs:flex-col primary-menu-list flex flex-row items-center h-full max-md:justify-center lg:ml-6 gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
-        'menu_class' => 'xs:whitespace-nowrap xs:flex-col primary-menu-list flex flex-row items-center h-full justify-center mb:gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
-        'add_li_class' => 'hover:text-hover text-desktopLink',
-        'after' => '<span class="max-sm:mr-[8px] sm:mx-[12px] text-primary"> | </span>'
+        'menu_class' => 'xs:whitespace-nowrap xs:flex-col primary-menu-list flex flex-col flex-wrap items-start h-full max-md:justify-center lg:ml-6 gap-2 font-bold lg:justify-start [&>li:nth-child(2)>span]:hidden ',
+        'add_li_class' => 'hover:text-hover text-desktopH2',
     )) ?>
+
 
     <div class="w-full">
         <img class="w-full h-full shrink-0" src="<?= get_template_directory_uri(); ?>/src/biography.png" alt="Joanna Helander">
