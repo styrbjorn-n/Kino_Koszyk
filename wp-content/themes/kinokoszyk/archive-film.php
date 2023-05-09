8095a2164a9f9b865c1f883e5f256ec26628ffa7
Styrbjörn Nordberg <styris.n@gmail.com>
2023-05-09 14:52:19 +0200

    removed z index from breadcrumbs

--- wp-content/themes/kinokoszyk/archive-film.php
+++ wp-content/themes/kinokoszyk/archive-film.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : ?>
-    <div class="flex relative mx-2 z-10 md:mx-16 lg:mx-[120px] breadcrumbs uppercase font-text text-grey lg:text-desktopLink md:text-tabletLink sm:text-mobileLink" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <div class="flex relative mx-2 md:mx-16 lg:mx-[120px] breadcrumbs uppercase font-text text-grey lg:text-desktopLink md:text-tabletLink sm:text-mobileLink" typeof="BreadcrumbList" vocab="https://schema.org/">
         <?php if (function_exists('bcn_display')) {
             bcn_display();
         } ?>
