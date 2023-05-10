8fb408a71027ea74ea777422bfaf33619fd2e03a
RubenOlander <rubenolander@gmail.com>
2023-05-10 14:21:33 +0200

    Update page-about.php

--- wp-content/themes/kinokoszyk/page-about.php
+++ wp-content/themes/kinokoszyk/page-about.php
@@ -8,34 +8,85 @@
     </div>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <h1 class="font-heading uppercase font-black mb-16 lg:text-desktopH1 md:text-tabletH1 max-md:mb-4 text-mobileH1"><?php the_title(); ?></h1>
-            <div class="w-full h-full max-md:mt-4">
-                <?php
-                $image = get_field('about_page_image');
-                if ($image) {
+            <section class="flex flex-row">
+                <div>
+                    <h2 class="mt-16 mb-6 font-heading text-desktopH2"><?php the_field('kino_koszyk_header') ?></h2>
+                    <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('kino_koszyk_description'); ?></p>
 
-                    $title = $image['title'];
-                    $alt = $image['alt'];
+                </div>
+                <div>
+                    <div class="w-full h-full max-md:mt-4">
+                        <?php
+                        $image = get_field('about_image_1');
+                        if ($image) {
 
-                    $size = 'large';
-                    $thumb = $image['sizes'][$size];
-                };
-                ?>
-                <img class="object-fill w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                            $title = $image['title'];
+                            $alt = $image['alt'];
+
+                            $size = 'large';
+                            $thumb = $image['sizes'][$size];
+                        };
+                        ?>
+                        <img class="object-fill w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                        <?php
+                        $image = get_field('about_image_2');
+                        if ($image) {
+
+                            $title = $image['title'];
+                            $alt = $image['alt'];
+
+                            $size = 'large';
+                            $thumb = $image['sizes'][$size];
+                        };
+                        ?>
+                        <img class="object-fill w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                    </div>
+                </div>
 
 
-            </div>
-            <h2 class="mt-16 mb-6 font-heading text-desktopH2"><?php the_field('kino_koszyk_header') ?></h2>
-            <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('kino_koszyk_description'); ?></p>
-            <h2 class="mb-1 font-heading lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 font-semibold"><?php the_field('joanna_helander_header') ?></h2>
-            <h4 class="mb-6 font-text lg:text-desktopH4 md:text-tabletH4 max-md:mobileH4 font-bold"><?php the_field('joanna_helander_subheader') ?></h4>
-            <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('joanna_helander_description') ?></p>
-            <a href="<?php the_field('joanna_helander_link'); ?>"><button class="bg-buttonRed px-2 h-11 text-white">Read more about Joanna</button></a>
-            <h2 class="mb-1 font-heading lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 font-semibold"><?php the_field('bo_persson_header') ?></h2>
-            <h4 class="mb-6 font-text lg:text-desktopH4 md:text-tabletH4 max-md:mobileH4 font-bold"><?php the_field('bo_persson_subheader') ?></h4>
-            <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('bo_persson_description') ?></p>
+            </section>
+            <section class="flex flex-row">
+
+                <div>
+                    <h2 class="mb-1 font-heading lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 font-semibold"><?php the_field('bo_persson_header') ?></h2>
+                    <h4 class="mb-6 font-text lg:text-desktopH4 md:text-tabletH4 max-md:mobileH4 font-bold"><?php the_field('bo_persson_subheader') ?></h4>
+                    <?php
+                    $image = get_field('bo_persson_image');
+                    if ($image) {
+
+                        $title = $image['title'];
+                        $alt = $image['alt'];
+
+                        $size = 'large';
+                        $thumb = $image['sizes'][$size];
+                    };
+                    ?>
+                    <img class="object-fill w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                    <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('bo_persson_description') ?></p>
+                </div>
+                <div>
+                    <h2 class="mb-1 font-heading lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 font-semibold"><?php the_field('joanna_helander_header') ?></h2>
+                    <h4 class="mb-6 font-text lg:text-desktopH4 md:text-tabletH4 max-md:mobileH4 font-bold"><?php the_field('joanna_helander_subheader') ?></h4>
+                    <?php
+                    $image = get_field('joanna_helander_image');
+                    if ($image) {
+
+                        $title = $image['title'];
+                        $alt = $image['alt'];
+
+                        $size = 'large';
+                        $thumb = $image['sizes'][$size];
+                    };
+                    ?>
+                    <img class="object-fill w-full" src="<?= esc_url($thumb); ?>" alt="<?= esc_attr($alt);  ?>" />
+                    <p class="mb-16 font-text text-desktopP max-w-[712px] lg:text-desktopH2 md:text-tabletH2 max-md:text-mobileH2 max-md:mt-4 lg:text-desktopH4 md:text-tabletH4 max-md:text-mobileH4 lg:text-desktopP md:text-tabletP max-md:text-mobileP sm:mt-2"><?php the_field('joanna_helander_description') ?></p>
+                    <a href="<?php echo get_page_link(1004); ?>"><button class="bg-buttonRed px-2 h-11 text-white">Read more about Joanna</button></a>
+
+                </div>
+            </section>
+
         <?php endwhile; ?>
     <?php endif; ?>
 </section>
-
 
 <?php get_footer(); ?>
